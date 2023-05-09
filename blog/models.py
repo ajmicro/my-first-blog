7523b8c6a3893d1eddcaaa4b8124d7adf50449a6
ajmicro <1ajros1@gmail.com>
2023-05-09 21:02:15 +0800

    Edit forms, models, blog, base html, post_edit html, views and database.

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,8 @@ from django.utils import timezone
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
+    account_officer = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
@@ -17,6 +18,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+    
+   
     
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
